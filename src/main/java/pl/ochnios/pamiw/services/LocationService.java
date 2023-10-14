104b249c5d142a03d3e2b0e0b2f2caf44deb02cf
Szymon <98458655+ochnios@users.noreply.github.com>
2023-10-14 14:13:42 +0200

    Extract object mapper from location service to shared package.

--- src/main/java/pl/ochnios/pamiw/services/LocationService.java
+++ src/main/java/pl/ochnios/pamiw/services/LocationService.java
@@ -3,33 +3,23 @@ package pl.ochnios.pamiw.services;
 import com.fasterxml.jackson.databind.ObjectReader;
 import pl.ochnios.pamiw.Consts;
 import pl.ochnios.pamiw.models.location.Location;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import pl.ochnios.pamiw.services.shared.HttpClientUtil;
+import pl.ochnios.pamiw.services.shared.ObjectMapperUtil;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
 public class LocationService {
-    private final ObjectMapper om;
     private Location[] foundLocations;
     private String[] foundCities;
-
-    public LocationService() {
-        om = new ObjectMapper();
-    }
 
     public String[] searchLocations(String searchPhrase) throws Exception {
         URI searchURI = createSearchLocationURI(searchPhrase);
         String locationsJson = HttpClientUtil.makeHttpRequest(searchURI);
 
-        foundLocations = om.readValue(locationsJson, Location[].class);
+        foundLocations = ObjectMapperUtil.getObjectMapper().readValue(locationsJson, Location[].class);
         foundCities = getCities(foundLocations);
 
         return foundCities;
