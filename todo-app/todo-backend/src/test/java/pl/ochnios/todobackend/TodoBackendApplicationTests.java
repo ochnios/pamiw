375eb2832c214f3762fff733f9fad20ea5992c94
Szymon Ochnio <szymonochnio15@gmail.com>
2023-11-03 16:18:36 +0100

    Add initial project structure generated by initializr.

---
+++ todo-app/todo-backend/src/test/java/pl/ochnios/todobackend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ochnios.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
