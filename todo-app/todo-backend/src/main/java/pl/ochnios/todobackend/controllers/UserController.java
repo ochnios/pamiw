6396414dd53efbe8eae6494893872835dfe552a2
Szymon Ochnio <szymonochnio15@gmail.com>
2023-11-12 23:56:45 +0100

    Add handling user views with thymeleaf.

--- todo-app/todo-backend/src/main/java/pl/ochnios/todobackend/controllers/UserController.java
+++ todo-app/todo-backend/src/main/java/pl/ochnios/todobackend/controllers/UserController.java
@@ -1,17 +1,20 @@
 package pl.ochnios.todobackend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import pl.ochnios.todobackend.Consts;
 import pl.ochnios.todobackend.dtos.UserDto;
+import pl.ochnios.todobackend.models.User;
 import pl.ochnios.todobackend.services.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RequestMapping("/api/users")
-@RestController
+@RequestMapping("/users")
+@Controller
 public class UserController {
 
     private final UserService userService;
@@ -21,37 +24,63 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDto> get(@PathVariable int id) {
-        UserDto user = UserDto.mapToDto(userService.getUser(id));
-        return ResponseEntity.ok(user);
-    }
-
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAll(@RequestParam(required = false) Integer page) {
-        int pageNumber = page != null && page >= 0 ? page : 0;
+    public String getPaginated(@RequestParam(required = false) Integer pageNumber,
+                               @RequestParam(required = false) String sortField,
+                               @RequestParam(required = false) String sortDirection,
+                               Model model) {
+
+        pageNumber = pageNumber != null && pageNumber >= 1 ? pageNumber : 1;
+        sortField = sortField != null ? sortField : "id";
+        sortDirection = sortDirection != null ? sortDirection : "asc";
+
+        Page<User> page = userService.getPaginatedUsers(pageNumber, Consts.PAGE_SIZE, sortField, sortDirection);
+
         List<UserDto> users = new ArrayList<>();
+        page.getContent().forEach((x) -> users.add(UserDto.mapToDto(x)));
 
-        userService.getPaginatedUsers(pageNumber).forEach((user) -> users.add(UserDto.mapToDto(user)));
+        model.addAttribute("currentPage", pageNumber);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDirection", sortDirection);
+        model.addAttribute("reverseSortDir", sortDirection.equals("asc") ? "desc" : "asc");
 
-        return !users.isEmpty() ? ResponseEntity.ok(users) : ResponseEntity.noContent().build();
+        model.addAttribute("users", users);
+
+        return "/users/users.html";
     }
 
-    @PostMapping
-    public ResponseEntity<UserDto> create(@RequestBody UserDto dto) {
-        UserDto createdUser = UserDto.mapToDto(userService.createUser(userService.mapFromDto(dto)));
-        return new ResponseEntity<UserDto>(createdUser, HttpStatus.CREATED);
+    @GetMapping("/create")
+    public String getCreationForm(Model model) {
+        UserDto user = new UserDto();
+        model.addAttribute("user", user);
+        return "/users/create.html";
     }
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<UserDto> update(@PathVariable int id, @RequestBody UserDto dto) {
-        UserDto updatedUser = UserDto.mapToDto(userService.updateUser(id, userService.mapFromDto(dto)));
-        return ResponseEntity.ok(updatedUser);
+    @GetMapping("/update/{id}")
+    public String getUpdateForm(@PathVariable int id, Model model) {
+        UserDto user = UserDto.mapToDto(userService.getUser(id));
+        model.addAttribute("user", user);
+        return "/users/update.html";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable int id) {
-        userService.deleteUser(id);
-        return ResponseEntity.ok().build();
+    @PostMapping("/save")
+    public String saveUser(@ModelAttribute("user") UserDto user) {
+        if (user.getId() == 0) {
+            // todo: fix error when users with the same name is added
+            userService.createUser(userService.mapFromDto(user));
+        } else {
+            userService.updateUser(user.getId(), userService.mapFromDto(user));
+        }
+
+        return "redirect:/users";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") int id) {
+        // todo: fix error when we want to delete user which is assigned to existing task
+        this.userService.deleteUser(id);
+        return "redirect:/users";
     }
 }
