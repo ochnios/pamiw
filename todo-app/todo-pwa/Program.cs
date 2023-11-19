a052c10ea2196401281f4a8847f95240572e03a7
Szymon Ochnio <szymonochnio15@gmail.com>
2023-11-20 00:14:58 +0100

    Parametrize backend BaseUrl in appsettings.json.

--- todo-app/todo-pwa/Program.cs
+++ todo-app/todo-pwa/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient() { BaseAddress = new Uri("http://localhost:8080") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetValue<String>("BaseAPIUrl")) });
 
 await builder.Build().RunAsync();
