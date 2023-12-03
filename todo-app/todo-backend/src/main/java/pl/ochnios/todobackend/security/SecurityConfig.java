fc0688b566deb730bf7b2892ed1de83139a79904
Szymon Ochnio <szymonochnio15@gmail.com>
2023-12-04 00:10:27 +0100

    Disable CORS...

--- todo-app/todo-backend/src/main/java/pl/ochnios/todobackend/security/SecurityConfig.java
+++ todo-app/todo-backend/src/main/java/pl/ochnios/todobackend/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable) // for development
+                .cors(Customizer.withDefaults()) // for development
                 .csrf(AbstractHttpConfigurer::disable) // for development
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(authEntryPoint)
