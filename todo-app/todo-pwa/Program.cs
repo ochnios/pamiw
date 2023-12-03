739eb3ea435d63014c36dba3de635c5ef0e5fd45
Szymon Ochnio <szymonochnio15@gmail.com>
2023-12-04 00:56:23 +0100

    Some comment

--- todo-app/todo-pwa/Program.cs
+++ todo-app/todo-pwa/Program.cs
@@ -2,10 +2,11 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using todo_pwa;
 using todo_pwa.Authentication;
 using todo_pwa.Services;
+
+// helpful repo https://github.com/codingdroplets/BlazorServerAuthenticationAndAuthorization/blob/master/Pages/Login.razor
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
